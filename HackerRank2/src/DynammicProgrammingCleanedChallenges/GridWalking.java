33a9cfd2caa6ad1591c436457aef52052752bc4f
CSCSCS84 <christoph.stettin@gmx.net>
2016-05-05 22:21:35 +0200

    Refactoring of GridWalking

--- HackerRank2/src/DynammicProgrammingCleanedChallenges/GridWalking.java
+++ HackerRank2/src/DynammicProgrammingCleanedChallenges/GridWalking.java
@@ -4,53 +4,172 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * Implementation of an algorithm for the GridWalking Problem
+ * https://www.hackerrank.com/challenges/grid-walking The Algorithmus works as
+ * follows: 1. Let i be one of dimensions. We calculated the number of ways with
+ * j=1,..,M steps where we only make a step in dimension i. 2. We calculate all
+ * posibilities in the dimension for making excactly M steps. 3. We multiply
+ * somhow the results from 1. and 2.
+ * 
+ * Explanation for 2. and 3.: Let N=3 and M=10. So we could do x0=3 steps in
+ * dimension 0, x1=3 in dimension 1 and x2=4 in dimension 2. In 1. we calculated
+ * the number of ways with length k for this dimension. So we multiply these 3
+ * numbers. Note, that there is some combinatoric needed since we could do the
+ * steps in an arbitrary order. Note, that we have to calculate all posibilities
+ * where x0+x1+x2=10 with x0,x1,x2 is the number of steps in each dimension.
+ */
 public class GridWalking {
-	static Scanner sc;
+	static Scanner scanner;
 	static int numberOfTestCases;
 	static byte N;
 	static short M;
 	byte border[];
-	long memoryCurPos[][];
-	long memoryNumOfPathForDimensionLengthOfPath[][];
-	static long sol = 0;
+	// stores for a dimension i the number of ways with length k starting from
+	// position j in [j][k]
+	long memoryNumOfWaysForPosition[][];
+	// stores for dimension i the number of path with length j, 1<=j<=M in
+	// [i][j]
+	long memoryNumOfPathForDimensionNumOfSteps[][];
 	static long modulo = 1000000007;
-	static long memoryWays[][];
-	
-	static byte[] position;
+	// needed for the calculations in step 2 and 3. stores in [i][j] the
+	// numofcombination when we have to make M-i steps in the dimensions
+	// i+1,...,N
+	static long memoryNumOfCombination[][];
+
+	static byte[] startPosition;
 
 	public static void main(String[] args) throws FileNotFoundException {
+
 		String pathOfTestCaseFile = "/home/christoph/Development2/HackerRank2/TestData/GridWalking/GridWalkingTest03.txt";
 		File file = new File(pathOfTestCaseFile);
-		sc = new Scanner(file);
-		numberOfTestCases = sc.nextInt();
-		GridWalking gr = new GridWalking();
-		for (int t = 1; t <= numberOfTestCases; t++) {
+		scanner = new Scanner(file);
+		numberOfTestCases = scanner.nextInt();
 
+		for (int t = 1; t <= numberOfTestCases; t++) {
+			GridWalking gr = new GridWalking();
+			gr.readInputForTestcase();
 			long result = gr.solve();
 			System.out.println(result);
-			sol = 0;
-
 		}
-
 	}
 
 	private long solve() {
-		readInput();
-		long result = 0;
-		memoryNumOfPathForDimensionLengthOfPath = new long[N][M];
-		for (byte dimension = 0; dimension < N; dimension++) {
-			for (int numOfSteps = 1; numOfSteps <= M; numOfSteps++) {
-				memoryCurPos = new long[border[dimension]][numOfSteps];
-				result = solveForCurrentPos(position[dimension], (short) numOfSteps, dimension);
 
-				memoryNumOfPathForDimensionLengthOfPath[dimension][numOfSteps - 1] = result;
-			}
-
-		}
-		memoryWays = new long[N][M + 1];
-		result = calcSubsetsLengthK((short) M, 0, 1);
+		calcNumOfWaysForEachDimension();
+		memoryNumOfCombination = new long[N][M + 1];
+		long result = calcNumOfWays(M, 0);
 
 		return result;
+	}
+
+	/**
+	 * Calculates for each dimension i the number of ways of length j=1,...,M
+	 * and stores it in memoryNumOfPathForDimensionLengthOfPath[i][j]
+	 */
+	private void calcNumOfWaysForEachDimension() {
+
+		memoryNumOfPathForDimensionNumOfSteps = new long[N][M];
+		for (int dimension = 0; dimension < N; dimension++) {
+			for (int numOfSteps = 1; numOfSteps <= M; numOfSteps++) {
+				memoryNumOfWaysForPosition = new long[border[dimension]][numOfSteps];
+				long result = solveForCurrentPos(startPosition[dimension], (short) numOfSteps, dimension);
+				memoryNumOfPathForDimensionNumOfSteps[dimension][numOfSteps - 1] = result;
+			}
+		}
+	}
+
+	private long calcNumOfWays(short numberOfOfElementsForSubset, int dimension) {
+		// TODO dimension==N needed?
+		if (numberOfOfElementsForSubset == 0 || dimension == N) {
+			return 1;
+		}
+
+		short minNumOfStepForDimension = 0;
+		if (dimension == N - 1) {
+			minNumOfStepForDimension = numberOfOfElementsForSubset;
+		}
+
+		long numOfWays = 0;
+		long bio = 1;
+		for (short i = numberOfOfElementsForSubset; i >= minNumOfStepForDimension; i--) {
+			bio = updateBio(numberOfOfElementsForSubset, bio, i);
+
+			long waysForElement  = waysForElement(i, dimension, bio);
+			long waysForNextSubset = 0;
+			
+			if (memoryNumOfCombination[dimension][numberOfOfElementsForSubset - i] != 0) {
+				waysForNextSubset = memoryNumOfCombination[dimension][numberOfOfElementsForSubset - i];
+
+			} else {
+				waysForNextSubset = calcNumOfWays((short) (numberOfOfElementsForSubset - i), dimension + 1) % modulo;
+				memoryNumOfCombination[dimension][numberOfOfElementsForSubset - i] = waysForNextSubset;
+			}
+			waysForElement = (waysForElement * waysForNextSubset) % modulo;
+			numOfWays = (numOfWays + waysForElement) % modulo;
+		}
+		return numOfWays;
+	}
+	private long waysForElement(int i,int dimension,long bio){
+		long numOfPathForEl = 1;
+		if (i != 0) {
+			numOfPathForEl = memoryNumOfPathForDimensionNumOfSteps[dimension][i - 1];
+		}
+		return (numOfPathForEl * bio) % modulo;
+	}
+
+	private long updateBio(short numberOfOfElementsForSubset, long bio, short i) {
+		if (i != numberOfOfElementsForSubset) {
+
+			bio = moduloDivision((bio * (i + 1)) % modulo, numberOfOfElementsForSubset - i, modulo);
+		} else {
+			bio = 1;
+		}
+		return bio;
+	}
+
+	private long solveForCurrentPos(byte currentPosition, short numOfSteps, int dimension) {
+		if (numOfSteps == 0) {
+			return 1;
+		}
+		long numOfWays = 0;
+		if (currentPosition + 1 <= border[dimension]) {
+			if (memoryNumOfWaysForPosition[currentPosition + 1 - 1][numOfSteps - 1] == 0) {
+				long a = solveForCurrentPos((byte) (currentPosition + 1), (short) (numOfSteps - 1), dimension);
+				numOfWays = (numOfWays + a) % modulo;
+				memoryNumOfWaysForPosition[currentPosition + 1 - 1][numOfSteps - 1] = a;
+			} else {
+				numOfWays = (numOfWays + memoryNumOfWaysForPosition[currentPosition + 1 - 1][numOfSteps - 1]) % modulo;
+			}
+		}
+
+		if (currentPosition - 1 >= 1) {
+
+			if (memoryNumOfWaysForPosition[currentPosition - 1 - 1][numOfSteps - 1] == 0) {
+				long a = solveForCurrentPos((byte) (currentPosition - 1), (short) (numOfSteps - 1), dimension);
+				numOfWays = (numOfWays + a) % modulo;
+				memoryNumOfWaysForPosition[currentPosition - 1 - 1][numOfSteps - 1] = a;
+			} else {
+				numOfWays = (numOfWays + memoryNumOfWaysForPosition[currentPosition - 1 - 1][numOfSteps - 1]) % modulo;
+			}
+		}
+
+		return numOfWays;
+	}
+
+	private void readInputForTestcase() {
+		N = scanner.nextByte();
+		M = scanner.nextShort();
+
+		startPosition = new byte[N];
+		for (int i = 0; i < N; i++) {
+			startPosition[i] = scanner.nextByte();
+		}
+		border = new byte[N];
+		for (int i = 0; i < N; i++) {
+			border[i] = scanner.nextByte();
+		}
+
 	}
 
 	private long moduloDivision(long numerator, long denominator, long modulo) {
@@ -75,95 +194,5 @@ public class GridWalking {
 			n = n / 2;
 		}
 		return result;
-	}
-
-	private long calcSubsetsLengthK(short numberOfOfElementsForSubset, int dimension, long mult) {
-
-		if (numberOfOfElementsForSubset == 0 || dimension == N) {
-
-			return 1;
-		}
-		long ways = 0;
-		short a = 0;
-
-		if (dimension == N - 1) {
-			a = (short) (numberOfOfElementsForSubset);
-		}
-		long bio = 1;
-		for (Short i = (short) (numberOfOfElementsForSubset); i >= a; i--) {
-			long waysForElement = 1;
-			long numOfPathForEl = 1;
-			long multCopy = mult;
-			long multi = 1;
-			if (i != 0) {
-				numOfPathForEl = memoryNumOfPathForDimensionLengthOfPath[dimension][i - 1];
-				if (i != M) {
-
-					if (i != numberOfOfElementsForSubset) {
-
-						bio = moduloDivision((bio * (i + 1)) % modulo, numberOfOfElementsForSubset - i, modulo);
-					}
-					multi = bio;
-				}
-
-			}
-			waysForElement = (numOfPathForEl * multi) % modulo;
-			long waysForNextSubset = 0;
-			if (memoryWays[dimension][numberOfOfElementsForSubset - i] != 0) {
-				waysForNextSubset = memoryWays[dimension][numberOfOfElementsForSubset - i];
-
-			} else {
-				waysForNextSubset = calcSubsetsLengthK((short) (numberOfOfElementsForSubset - i), dimension + 1,
-						multCopy) % modulo;
-				memoryWays[dimension][numberOfOfElementsForSubset - i] = waysForNextSubset;
-			}
-			waysForElement = (waysForElement * waysForNextSubset) % modulo;
-			ways = (ways + waysForElement) % modulo;
-		}
-		return ways;
-	}
-
-	private long solveForCurrentPos(byte currentPosition, short numOfSteps, byte dimension) {
-		if (numOfSteps == 0) {
-			return 1;
-		}
-		long numOfWays = 0;
-		if (currentPosition + 1 <= border[dimension]) {
-			if (memoryCurPos[currentPosition + 1 - 1][numOfSteps - 1] == 0) {
-				long a = solveForCurrentPos((byte) (currentPosition + 1), (short) (numOfSteps - 1), dimension);
-				numOfWays = (numOfWays + a) % modulo;
-				memoryCurPos[currentPosition + 1 - 1][numOfSteps - 1] = a;
-			} else {
-				numOfWays = (numOfWays + memoryCurPos[currentPosition + 1 - 1][numOfSteps - 1]) % modulo;
-			}
-		}
-
-		if (currentPosition - 1 >= 1) {
-
-			if (memoryCurPos[currentPosition - 1 - 1][numOfSteps - 1] == 0) {
-				long a = solveForCurrentPos((byte) (currentPosition - 1), (short) (numOfSteps - 1), dimension);
-				numOfWays = (numOfWays + a) % modulo;
-				memoryCurPos[currentPosition - 1 - 1][numOfSteps - 1] = a;
-			} else {
-				numOfWays = (numOfWays + memoryCurPos[currentPosition - 1 - 1][numOfSteps - 1]) % modulo;
-			}
-		}
-
-		return numOfWays;
-	}
-
-	private void readInput() {
-		N = sc.nextByte();
-		M = sc.nextShort();
-
-		 position = new byte[N];
-		for (int i = 0; i < N; i++) {
-			position[i] = sc.nextByte();
-		}
-		border = new byte[N];
-		for (int i = 0; i < N; i++) {
-			border[i] = sc.nextByte();
-		}
-
 	}
 }
